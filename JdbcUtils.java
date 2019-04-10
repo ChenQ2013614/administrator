21db6ce58f8c37f19cd7b9badd8bc72aad82d989
陈琼 <48155449+ChenQ2013614@users.noreply.github.com>
2019-04-10 21:43:27 +0800

    Add files via upload

---
+++ JdbcUtils.java
@@ -0,0 +1,10 @@
+package com.jdbc;
+
+public class JdbcUtils {
+	private final String USERNAME = "root";
+	private final String PASSWORD = "123456";
+	private final String DRIVER = "shuiguo.mysql.jdbc.Driver";
+	private final String URL = "jdbc:mysql://localhost:3306/mydb";
+
+
+}
