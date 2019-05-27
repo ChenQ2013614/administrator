dd87b17e906a4ed144901789fb69826fe23be116
陈琼 <48155449+ChenQ2013614@users.noreply.github.com>
2019-05-27 15:51:01 +0800

    Update Book.java

--- src/org/fkit/shoppingApp/bean/Book.java
+++ src/org/fkit/shoppingApp/bean/Book.java
@@ -6,21 +6,19 @@ package org.fkit.shoppingApp.bean;
 import java.util.Date;
 
 /**
- * @author 罗春龙
+ * @author 亲爱的琼
  * Version 1.0
- * 2018年10月11日
- * 用于封装书籍信息
  */
 public class Book {
 	
-	private Integer id;//用户id
-	private String name;//书籍名字
-	private String author;//作者
-	private Date publicationdate;//出版时间
-	private String publication;//出版社
-	private Double price;//价格
-	private String image;//图片地址|图片名称
-	private String remark;//备注信息
+	private Integer id;
+	private String name;
+	private String author;
+	private Date publicationdate;
+	private String publication;
+	private Double price;
+	private String image;
+	private String remark;
 	
 	
 
