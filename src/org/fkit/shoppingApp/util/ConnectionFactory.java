4d1e81337ea6ac7332d6867d0ac97f3bc83b3a19
陈琼 <48155449+ChenQ2013614@users.noreply.github.com>
2019-05-27 16:01:42 +0800

    Update ConnectionFactory.java

--- src/org/fkit/shoppingApp/util/ConnectionFactory.java
+++ src/org/fkit/shoppingApp/util/ConnectionFactory.java
@@ -16,44 +16,17 @@ import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
 
 
 /**
- * @author 罗春龙
+ * @author 亲爱的琼
  * Version 1.0
- * 2018年10月12日
- *  连接工厂
  */
 public class ConnectionFactory {
 	
 	static DataSource basicDataSource = new BasicDataSource();
-	//对数据库连接池进行初始化
-	/*static {
-
-		//设置驱动信息
-		basicDataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		//设置连接的地址
-		basicDataSource.setUrl("jdbc:mysql://127.0.0.1:3306/bookapp");
-		//指定数据连接的账号
-		basicDataSource.setUsername("root");
-		//指定数据库连接密码
-		basicDataSource.setPassword("123");
-		
-		//设置最大连接数
-		basicDataSource.setMaxActive(3);
-		
-		//设置连接保存数
-		basicDataSource.setMaxIdle(3);
-		
-		//设置最大等待时间   单位毫秒
-		basicDataSource.setMaxWait(5000);
-	}*/
 	
 	static {
        try {
     	   Context initCtx = new InitialContext();
-    	   //java:/comp/env  代表Tomcat 容器的前缀        不同的JNDI容器前缀不一样
-   		  // Context envCtx = (Context) initCtx.lookup("java:/comp/env");
-   		   //jdbc/bookApp  被检索的资源的名称
-   		  // basicDataSource = (DataSource)envCtx.lookup("jdbc/bookApp");
-   		   
+    	   
    		   
     	   basicDataSource = (DataSource)initCtx.lookup("java:/comp/env/jdbc/bookApp");
    		
